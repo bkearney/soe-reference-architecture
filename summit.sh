5386940d72c4172009e96a9a91c4de25ffe69ea2
root <root@dhcp137-206.rdu.redhat.com>
2015-06-02 14:33:04 -0400

    add in a summit piece

---
+++ summit.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+# this is step where we expect to have an already installed satellite 6 and create the configuration for all further steps, the main org and subscriptions
+
+# TODO short desc and outcome of this step
+
+DIR="$PWD"
+source "${DIR}/common.sh"
+
+# check if organization already exists. if yes, exit
+hammer organization info --name "$ORG" >/dev/null 2>&1 
+if $? -ne 0; then  
+  echo "Organization $ORG already exists. Exit."
+  exit 1
+fi
+
+# create org
+hammer organization create --name "$ORG" --label "$ORG_LABEL" --description "$ORG_DESCRIPTION"
+
+if [ -f $subscription_manifest_loc ]; then
+# upload manifest
+hammer subscription upload --organization "$ORG" --file "$subscription_manifest_loc"
+else
+ echo "please upload your manifest to the Satellite Server and specify the location in $HOME/.soe-config"
+ exit 1
+fi
+
+# note: has worked for me only at the second try, so maybe we should check if successful before proceeding:
+hammer subscription list --organization "$ORG" | ( grep -q 'Red Hat' && echo ok ) || ( echo "Subscription import has not been successful. Exit"; exit 1 )
+
